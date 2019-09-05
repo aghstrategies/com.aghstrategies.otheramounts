d2d3d0e734954efaccea6d695947870eb4e189d9
Alice Frumin <alice@aghtrategies.com>
2019-09-05 17:28:58 -0400

    use box instead of the radio option

--- js/otherAmount.js
+++ js/otherAmount.js
@@ -2,40 +2,48 @@ CRM.$(function ($) {
   var otherFields = CRM.vars.otheramounts.otherFields;
 
   $.each(otherFields, function(priceFieldId, priceOptionId) {
-    var $fieldName = '#other_amount_' + priceFieldId;
-    var $otherAmountDiv = $('#other_amount_' + priceFieldId).parent().parent();
+    // id of the box being used for other amounts for this field
+    var $otherBox = '#other_amount_' + priceFieldId;
+
+    // shorcut for the price name
+    var priceField = 'price_' + priceFieldId;
+    var inputId = '#' + $('[name="price_' + priceFieldId + '"][value="' + priceOptionId + '"]').attr('id');
 
     // Put other amount fields under their related price fields
-    $($otherAmountDiv).insertAfter($('[name^="price_' + priceFieldId + '"]').parent().parent().parent().last());
+    $($otherBox).parent().parent().insertAfter($('[name^="price_' + priceFieldId + '"]').parent().parent().parent().last());
 
-    var inputId = '#' + $('[name="price_' + priceFieldId + '"][value="' + priceOptionId + '"]').attr('id');
-    var priceField = 'price_' + priceFieldId;
 
-    var showOtherAmountBox = function () {
-      if ($('input[name=' + priceField + ']:checked').val() == $(inputId).val()) {
-        $otherAmountDiv.css('display', 'block');
-      } else {
-        $otherAmountDiv.css('display', 'none');
-        $('input#other_amount_' + priceFieldId).val('');
-      }
+    // Hide other amount option just rely on the box
+    $(inputId).parent().hide();
+
+    // Use the other amount option which is hidden
+    var useOtherField = function () {
+      $(inputId).prop('checked', true);
+      $(inputId).trigger('change');
+      $(inputId).trigger('click');
     };
 
-    showOtherAmountBox();
-    $('input[name=' + priceField + ']').change(showOtherAmountBox);
+    useOtherField();
+    $($otherBox).keyup(useOtherField);
+    $($otherBox).click(useOtherField);
 
+    // Update the Total Amount 
     var calculateTotalWithOtherAmount = function () {
       if ($('input[name=' + priceField + ']:checked').val() == $(inputId).val()) {
         var a = 0;
-        if (parseFloat($('input' + $fieldName).val()) > 0) {
-          var a = parseFloat($('input' + $fieldName).val());
+        if (parseFloat($('input' + $otherBox).val()) > 0) {
+          var a = parseFloat($('input' + $otherBox).val());
         }
         $('input[name=' + priceField + ']:checked').attr('price', '["' + priceField + '", "' + a + '||"]');
         $('input[name=' + priceField + ']:checked').trigger('click');
+      }
+      else {
+        $('input#other_amount_' + priceFieldId).val('');
       }
     };
 
     calculateTotalWithOtherAmount();
     $('input[name^=other_amount_' + priceFieldId + ']').keyup(calculateTotalWithOtherAmount);
-    $('input[name^="price_' + priceFieldId + '"]').change(calculateTotalWithOtherAmount);
+    $('input[name^=' + priceField + ']').change(calculateTotalWithOtherAmount);
   });
 });
