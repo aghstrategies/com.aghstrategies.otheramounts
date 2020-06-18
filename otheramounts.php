c8c8410025ec90aaaa087d2a12e6f89d4f3ce0cb
tommybobo <tommy@aghstrategies.com>
2020-06-18 11:51:57 -0400

    update label

--- otheramounts.php
+++ otheramounts.php
@@ -34,7 +34,7 @@ function otheramounts_civicrm_buildform($formName, &$form) {
           $otherAmounts = TRUE;
           foreach ($fieldDetails['options'] as $key => $values) {
             // TODO: handle this differently than looking for the label
-            if ($values['label'] == 'Pay What You Can') {
+            if ($values['label'] == 'Pay What You Can (enter amount below--minimum $10)') {
               $detsForJs[$fieldId] = $key;
             }
           }
@@ -66,7 +66,7 @@ function otheramounts_civicrm_buildAmount($pageType, &$form, &$amount) {
       if (in_array($fieldId, $fieldsToAddOtherAmountOptionFor) && !empty($form->_submitValues["other_amount_$fieldId"])) {
         $otherAmounts = TRUE;
         foreach ($fieldDetails['options'] as $optionId => $values) {
-          if ($values['label'] == 'Pay What You Can') {
+          if ($values['label'] == 'Pay What You Can (enter amount below--minimum $10)') {
             $amount[$fieldId]['options'][$optionId]['amount'] = $form->_submitValues["other_amount_$fieldId"];
           }
         }
